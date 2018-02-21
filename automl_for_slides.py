471ad4926674e5d0bdd4c52d8e153c96d18665c3
viktor dashkov <viktordashkov@viktors-MacBook-Pro.local>
2018-02-20 20:47:07 -0800

    saved model

--- automl_for_slides.py
+++ automl_for_slides.py
@@ -5,5 +5,9 @@ h2o.init()
 aml = H2OAutoML(max_runtime_secs=3600)  # 1 hour
 aml.train(y="SalePrice", training_frame=h2o.import_file('/housing_train.csv'))
 
+
+
+
+
 leader = aml.leader
 prediction = leader.predict(h2o.import_file('/housing_predict.csv'))
